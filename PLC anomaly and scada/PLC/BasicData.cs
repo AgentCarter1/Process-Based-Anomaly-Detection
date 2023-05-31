d8d3f8e590f7fad7094b6d87faebe3d5f9f2a5f8
Mustafa Burak Aydın <62963891+AgentCarter1@users.noreply.github.com>
2023-05-31 16:41:43 +0300

    Add files via upload

---
+++ PLC anomaly and scada/PLC/BasicData.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharp7;
+using System.Windows.Forms;
+using static Sharp7.S7Client;
+namespace PLC
+{
+    public static class BasicData
+    {
+
+        public static void AnomalyDetection(S7Client plc1,S7Client plc2, S7Client plc3,ListBox lstBox)
+        {
+            byte[] db1Buffer = new byte[18];
+            byte[] db2Buffer = new byte[18];
+            byte[] db3Buffer = new byte[18];
+            int result1 = plc1.DBRead(1, 0, 18, db1Buffer);
+            int result2 = plc2.DBRead(1, 0, 18, db2Buffer);
+            int result3 = plc3.DBRead(1, 0, 18, db3Buffer);
+            float currentlevel1 = 0, currentlevel2 = 0, currentlevel3 = 0;
+            if (result1 != 0)
+            {
+                MessageBox.Show(plc1.ErrorText(result1), "Error", MessageBoxButtons.OK);
+            }else
+            {
+                currentlevel1= S7.GetRealAt(db1Buffer, 2);
+            }
+
+            if (result2 != 0)
+            {
+                MessageBox.Show(plc2.ErrorText(result2), "Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                currentlevel2 = S7.GetRealAt(db2Buffer, 4);
+            }
+
+            if (result3 != 0)
+            {
+                MessageBox.Show(plc3.ErrorText(result3), "Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                currentlevel3 = S7.GetRealAt(db3Buffer, 4);
+            }
+
+            if (Convert.ToUInt32(currentlevel1) < 10 && (Convert.ToUInt32(currentlevel2) > 50))
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 20 && Convert.ToUInt32(currentlevel2) > 60)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 30 && Convert.ToUInt32(currentlevel2) > 70) {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 40 && Convert.ToUInt32(currentlevel2) > 80)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 10 && Convert.ToUInt32(currentlevel2) < 10)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 20 && Convert.ToUInt32(currentlevel2) < 20)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 30 && Convert.ToUInt32(currentlevel2) < 30)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel1) < 40 && Convert.ToUInt32(currentlevel2) < 40)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon1 ve İstasyon2'nin Su Seviyesini Kontrol Edin!");
+            }
+
+
+            else if (Convert.ToUInt32(currentlevel2) < 10 && Convert.ToUInt32(currentlevel3) > 50)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel2) < 20 && Convert.ToUInt32(currentlevel3) > 60)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel2) < 30 && Convert.ToUInt32(currentlevel3) > 70)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel2) > 40 && Convert.ToUInt32(currentlevel3) > 80)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+
+            else if (Convert.ToUInt32(currentlevel2) < 10 && Convert.ToUInt32(currentlevel3) < 10)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel2) < 20 && Convert.ToUInt32(currentlevel3) < 20)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel2) < 30 && Convert.ToUInt32(currentlevel3) < 30)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+            else if (Convert.ToUInt32(currentlevel2) < 40 && Convert.ToUInt32(currentlevel3) < 40)
+            {
+                lstBox.Items.Add((DateTime.Now).ToString() + "İstasyon2 ve İstasyon3'ün Su Seviyesini Kontrol Edin!");
+            }
+        }
+
+        public static void getData(S7Client plc,TextBox totalvolume,TextBox currentlevel,TextBox currentvolume,
+            ProgressBar progressBar1, Label labelProportion,Label localLabel, int result,int secondDb,int totalVolumeOff,
+            int currentVolumeOff,int currentLevelOff,int treatment,TextBox treatmentTextbox)
+        {
+            if (result == 0)
+            {
+
+                byte[] db1Buffer = new byte[18];
+                result = plc.DBRead(1, 0, 18, db1Buffer);
+                if (result != 0)
+                {
+                    MessageBox.Show(plc.ErrorText(result), "Error", MessageBoxButtons.OK);
+                }
+
+                float totalVolume = S7.GetRealAt(db1Buffer, totalVolumeOff);
+                totalvolume.Text = totalVolume.ToString();
+
+                float currentLevel = S7.GetRealAt(db1Buffer, currentLevelOff);
+                currentlevel.Text = currentLevel.ToString();
+                if(Convert.ToInt32(currentLevel)<0)
+                    progressBar1.Value = (Convert.ToInt32(currentLevel))*(-1);
+                else
+                    progressBar1.Value = Convert.ToInt32(currentLevel);
+                labelProportion.Text = "%" + progressBar1.Value.ToString();
+
+                float currentVolume = S7.GetRealAt(db1Buffer, currentVolumeOff);
+                currentvolume.Text = currentVolume.ToString();
+               
+                //for take data other table
+                result = plc.DBRead(secondDb, 0, 18, db1Buffer);
+                if (result != 0)
+                {
+                    MessageBox.Show(plc.ErrorText(result), "Error", MessageBoxButtons.OK);
+                }
+                bool local = S7.GetBitAt(db1Buffer, 0, 0);
+                localLabel.Text = local.ToString();
+
+                if(treatment != 0)
+                {
+                    int currentLvlToSend = S7.GetIntAt(db1Buffer, treatment);
+                    treatmentTextbox.Text = currentLvlToSend.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show(plc.ErrorText(result), "Error", MessageBoxButtons.OK);
+            }
+        }
+
+
+   
+
+       
+    }
+}
---
+++ PLC anomaly and scada/PLC/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sharp7;
+using static Sharp7.S7Client;
+namespace PLC
+{
+    public partial class Form1 : Form
+    {
+        static S7Client plc1 = new S7Client();
+        static S7Client plc2 = new S7Client();
+        static S7Client plc3 = new S7Client();
+        public int result1 = plc1.ConnectTo("192.168.41.10", 0, 1);
+        public int result = plc2.ConnectTo("192.168.42.10", 0, 1);
+        public int result3 = plc3.ConnectTo("192.168.43.10", 0, 1);
+        
+        int time = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void getPlc1Data(TextBox txt1,TextBox txt2,TextBox txt3,ProgressBar pgbar,Label lbl1,Label lbl2)
+        {
+            BasicData.getData(plc1, txt1, txt2, txt3, pgbar,
+                       lbl1, lbl2, result1, 8, 6, 10, 2, 0, treatmentwaterevac);
+            
+        }
+        private void getPlc2Data(TextBox txt1, TextBox txt2, TextBox txt3, ProgressBar pgbar, Label lbl1, Label lbl2)
+        {
+            BasicData.getData(plc2, txt1, txt2, txt3,
+                        pgbar, lbl1, lbl2, result, 16, 8, 12, 4, 0, treatmentwaterevac);
+        }
+        private void getPlc3Data(TextBox txt1, TextBox txt2, TextBox txt3, ProgressBar pgbar, Label lbl1, Label lbl2,TextBox txt4)
+        {
+            BasicData.getData(plc3, txt1, txt2, txt3,
+                    pgbar, lbl1, lbl2, result3,
+                    8, 8, 12, 4, 2, txt4);
+        }
+        private void setProgressBar(ProgressBar pgbar, int min, int max)
+        {
+            pgbar.Maximum = max;
+            pgbar.Minimum = min;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            tabControl1.SelectedIndexChanged += new EventHandler(tabControl1_SelectedIndexChanged);
+           
+            
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+            {
+                getPlc2Data(totalvolume, currentlevel, currentvolume,
+                        progressBar1, label14, label13);
+                time = 3;
+            }
+        }
+
+        
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab == promotion1)
+            {
+                if (result1 == 0)
+                {
+                    
+                    promotion1timer.Start();
+                    getPlc1Data(totalvolume1, currentlevel1, currentvolum1, progresspromotion2,
+                       label15, label16);
+                    setProgressBar(progresspromotion2, 0, 100);
+                    pumpflowmeter1.Text = "0";
+                }
+                else
+                {
+                    MessageBox.Show(plc1.ErrorText(result1), "Error", MessageBoxButtons.OK);
+                }
+            }
+            else if (tabControl1.SelectedTab == promotion2)
+            {
+                if (result == 0)
+                {
+                    
+                    promotion2timer.Start();
+                    getPlc2Data(totalvolume, currentlevel, currentvolume,
+                        progressBar1, label14, label13);
+                    
+                    setProgressBar(progressBar1, 0, 100);
+                    pumpvolume.Text = "0";
+                }
+                else
+                {
+                    MessageBox.Show(plc2.ErrorText(result), "Error", MessageBoxButtons.OK);
+                }
+            }
+            else if (tabControl1.SelectedTab == treatment)
+            {
+
+                if (result3 == 0)
+                {
+                    treatmenttimer.Start();
+                    getPlc3Data(treatmentTotalText, treatmentCurrentlvl, treatmentCurrentText,
+                    progressBar3, label26, label29, treatmentwaterevac);
+                    setProgressBar(progressBar3, 0, 100);
+                    treatmentFlowmeterText.Text = "0";
+                }
+                else
+                {
+                    MessageBox.Show(plc3.ErrorText(result3), "Error", MessageBoxButtons.OK);
+                }
+            }
+            else if (tabControl1.SelectedTab == alldata)
+            {
+                BasicData.AnomalyDetection(plc1, plc2, plc3,listBox1);
+                if (result1 == 0)
+                {
+                    
+                    allPro1.Start();
+                    getPlc1Data(textBox13, textBox10, textBox12, progressBar2,
+                       label43, label71);
+                    setProgressBar(progresspromotion2, 0, 100);
+                    pumpflowmeter1.Text = "0";
+                }
+                else
+                {
+                    MessageBox.Show(plc1.ErrorText(result1), "Error", MessageBoxButtons.OK);
+                }
+                if (result == 0)
+                {
+                    
+                    allPro2.Start();
+                    getPlc2Data(textBox4, textBox1, textBox3,
+                        progressBar5, label81, label44);
+
+                    setProgressBar(progressBar1, 0, 100);
+                    pumpvolume.Text = "0";
+                }
+                else
+                {
+                    MessageBox.Show(plc2.ErrorText(result), "Error", MessageBoxButtons.OK);
+                }
+                if (result3 == 0)
+                {
+                    alltreatment.Start();
+                    getPlc3Data(textBox9, textBox6, textBox8,
+                    progressBar4, label70, label60, textBox5);
+                    setProgressBar(progressBar3, 0, 100);
+                    treatmentFlowmeterText.Text = "0";
+                }
+                else
+                {
+                    MessageBox.Show(plc3.ErrorText(result3), "Error", MessageBoxButtons.OK);
+                }
+            }                 
+        }
+        private void promotion1timer_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+            {
+                time = 3;
+                getPlc1Data(totalvolume1, currentlevel1, currentvolum1, progresspromotion2,
+                       label15, label16);
+            }
+        }
+
+        private void treatmenttimer_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if(time == 0)
+            {
+                time = 3;
+                getPlc3Data(treatmentTotalText, treatmentCurrentlvl, treatmentCurrentText,
+                    progressBar3, label26, label29, treatmentwaterevac);
+            }
+        }
+
+        private void allPro1_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+            {
+                time = 3;
+                getPlc1Data(textBox13, textBox10, textBox12, progressBar2,
+                       label43, label71);
+            }
+        }
+
+        private void allPro2_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+            {
+                time = 3;
+
+                getPlc2Data(textBox4, textBox1, textBox3,
+                    progressBar5, label81, label44);
+            }
+        }
+
+        private void alltreatment_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+            {
+                time = 3;
+                getPlc3Data(textBox9, textBox6, textBox8,
+                    progressBar4, label70, label60, textBox5);
+            }
+            
+        }
+
+        private void promotion1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
